8924d6f647bed440d91a2277109d21387dd22acf
z4nix <45301484+z4nix@users.noreply.github.com>
2023-12-06 19:31:06 +0100

    Update multiple_linear_regression.py

--- src/multiple_linear_regression.py
+++ src/multiple_linear_regression.py
@@ -1,89 +1,49 @@
 import numpy as np
-from sklearn import datasets
-from regression_plotter import RegressionPlotter
-from model_saver import ModelSaver
-
 
 class MultipleLinearRegressor:
     def __init__(self, default_intercept=0, default_slope=0) -> None:
-        self.intercept = default_intercept
-        self.slope = default_slope
+        self._intercept = default_intercept  # Now a protected attribute
+        self._slope = default_slope          # Now a protected attribute
 
     def train(self, x: np.array, y: np.array) -> None:
         if not isinstance(x, np.ndarray) or not isinstance(y, np.ndarray):
             raise TypeError("x and y must be numpy arrays!")
 
-        xIntercept = np.hstack(
-            [np.ones((x.shape[0], 1)), x])  # adding a column of ones (to account for the intercept term)
+        xIntercept = np.hstack([np.ones((x.shape[0], 1)), x])
 
         try:
-            inverted = np.linalg.inv(np.dot(xIntercept.T, xIntercept)) #checking if the matrix is invertible
+            inverted = np.linalg.inv(np.dot(xIntercept.T, xIntercept))
         except np.linalg.LinAlgError:
             raise Exception("the matrix X^T X is singular and non-invertible!")
 
-        weights = np.dot(inverted, np.dot(xIntercept.T, y))  # computing the weights (w = (X^T * X)^-1 * X^T * y)
-        # above is the the dot product, of the inverse of the dot product, of the transpose of xIntercept and xIntercept and the dot product of the transpose of xIntercept and y
-        self.intercept = weights[0]  # setting  intercept to the first  element of the weights array
-        self.slope = weights[1:]  # setting slope to the rest of the elements of the weights array
+        weights = np.dot(inverted, np.dot(xIntercept.T, y))
+        self._intercept = weights[0]  # Update to protected attribute
+        self._slope = weights[1:]     # Update to protected attribute
 
     def predict(self, x: np.array) -> np.array:
         if not isinstance(x, np.ndarray):
             raise TypeError("x must be a numpy array!")
 
-        xIntercept = np.hstack([np.ones((x.shape[0], 1)), x])  # adding a column of ones (to account for the intercept term)
-        return np.dot(xIntercept, np.hstack([self.intercept,
-                                             self.slope]))  # returning the dot product of xIntercept and the weights array (the intercept and the slope) formula: y = w0 + w1*x1 + w2*x2 + ... + wn*xn
+        xIntercept = np.hstack([np.ones((x.shape[0], 1)), x])
+        return np.dot(xIntercept, np.hstack([self._intercept, self._slope]))
 
     def get_params(self):
         return {
-            'intercept': self.intercept,
-            'slope': self.slope.tolist() if hasattr(self.slope, "tolist") else self.slope  # convert to
+            'intercept': self._intercept,
+            'slope': self._slope.tolist() if hasattr(self._slope, "tolist") else self._slope
         }
 
     def set_params(self, params: dict) -> None:
         if not isinstance(params, dict):
             raise TypeError("params must be a dictionary!")
 
-        self.intercept = params['intercept']
-        self.slope = params['slope']
+        self._intercept = params['intercept']
+        self._slope = params['slope']
 
-
-if __name__ == "__main__":
-    model = MultipleLinearRegressor()
-
-    # test the model on the diabetes dataset
-    diabetes = datasets.load_diabetes()
-    diabetes_x = diabetes.data
-    diabetes_y = diabetes.target
-    diabetes_x = diabetes_x.astype(float)
-    model.train(diabetes_x, diabetes_y)
-    diabetes_y_pred = model.predict(diabetes_x)
-    print("Ground truth and predicted values:", diabetes_y, diabetes_y_pred, sep="\n")
-
-    # compare with sklearn
-    from sklearn.linear_model import LinearRegression
-
-    sklearn_model = LinearRegression()
-    sklearn_model.fit(diabetes_x, diabetes_y)
-    sklearn_y_pred = sklearn_model.predict(diabetes_x)
-    print("Ground truth and SKLEARN predicted values:", diabetes_y, sklearn_y_pred, sep="\n")
+    # Uncomment below to use the plotting functionality
 
     '''
-    # plot the regression lines for each feature
-    plotter = RegressionPlotter(model, diabetes.data, diabetes.target)
-    plotter.plot()
-    '''
-
-    '''
-    # save and load parameters
-    saver = ModelSaver(format='json')
-    saver.save_model(model, 'model_params.json')
-
-    saver.load_model('model_params.json', model)
-    '''
-
-    '''
-    # plotting the results of sklearn and of my own to make visual comparison using matplotlib
+    # plotting the results of sklearn and of our own to make visual comparison using matplotlib
 
     import matplotlib.pyplot as plt
 
